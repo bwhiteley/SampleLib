889ec04bbd80fefeffa67f9935c88022635e6824
Bart Whiteley <bwhiteley@gmail.com>
2018-05-03 16:31:32 -0600

    Initial Commit

---
+++ SampleLib/SampleLib.h
@@ -0,0 +1,19 @@
+//
+//  SampleLib.h
+//  SampleLib
+//
+//  Created by Bart Whiteley on 5/3/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleLib.
+FOUNDATION_EXPORT double SampleLibVersionNumber;
+
+//! Project version string for SampleLib.
+FOUNDATION_EXPORT const unsigned char SampleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleLib/PublicHeader.h>
+
+
